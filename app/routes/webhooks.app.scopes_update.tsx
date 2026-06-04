e22d803d20db93441569f84fbd9b6653f5a7540e
Jefferson Rosales <jefferson.rosales@au.harveynorman.com>
2026-06-04 14:09:30 +0800

    refactor: update webhook handling to use sessionStorage for session management

--- app/routes/webhooks.app.scopes_update.tsx
+++ app/routes/webhooks.app.scopes_update.tsx
@@ -1,22 +1,14 @@
 import type { ActionFunctionArgs } from "react-router";
-import { authenticate } from "../shopify.server";
+import { authenticate, sessionStorage } from "../shopify.server";
 
 export const action = async ({ request }: ActionFunctionArgs) => {
-  const { payload, session, topic, shop } = await authenticate.webhook(request);
-  console.log(`Received ${topic} webhook for ${shop}`);
+  const { payload, session } = await authenticate.webhook(request);
 
   const current = payload.current as string[];
   if (session) {
-    // TODO: Convert to redis implementation
-
-    // await db.session.update({
-    //     where: {
-    //         id: session.id
-    //     },
-    //     data: {
-    //         scope: current.toString(),
-    //     },
-    // });
+    session.scope = current.join(",");
+    await sessionStorage.storeSession(session);
   }
+
   return new Response();
 };
--- app/routes/webhooks.app.uninstalled.tsx
+++ app/routes/webhooks.app.uninstalled.tsx
@@ -1,17 +1,13 @@
 import type { ActionFunctionArgs } from "react-router";
-import { authenticate } from "../shopify.server";
+import { authenticate, sessionStorage } from "../shopify.server";
 
 export const action = async ({ request }: ActionFunctionArgs) => {
-  const { shop, session, topic } = await authenticate.webhook(request);
-
-  console.log(`Received ${topic} webhook for ${shop}`);
+  const { shop } = await authenticate.webhook(request);
 
   // Webhook requests can trigger multiple times and after an app has already been uninstalled.
   // If this webhook already ran, the session may have been deleted previously.
-  if (session) {
-    // TODO: Convert to redis implementation
-    // await db.session.deleteMany({ where: { shop } });
-  }
+  const sessions = await sessionStorage.findSessionsByShop(shop);
+  await sessionStorage.deleteSessions(sessions.map((s) => s.id));
 
   return new Response();
 };
